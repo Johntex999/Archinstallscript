0564a49017d63797d3a0165fe97e47e7cfb82914
Johntex999 <jonte.grunewald@proton.me>
2025-08-29 19:27:02 +0200

    Delete finish.sh

--- finish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo ">>> Starting archinstall script..."  # Debug message
-
-pacman -Sy archinstall -y
-archinstall --config user_configuration.json --creds user_credentials.json
