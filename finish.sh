ab9322851928f1dd763e090a3fb594181f019d0f
Johntex999 <jonte.grunewald@proton.me>
2025-08-29 18:50:04 +0200

    Update finish.sh

--- finish.sh
+++ finish.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 echo ">>> Starting archinstall script..."  # Debug message
 
-pacman -Sy archinstall
+pacman -Sy archinstall -y
 archinstall --config user_configuration.json --creds user_credentials.json
